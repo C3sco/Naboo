6070f8b27bbda3a27dd29b9e4c959b9b3748e732
C3sco <c3sco1@gmail.com>
2022-08-03 21:53:37 +0200

    Gson

---
+++ NABOO/src/application/Notizia.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.time.LocalDateTime;
+
+public class Notizia {
+	String titolo;
+	LocalDateTime timestamp;
+	String descrizione;
+	String autore;
+	String fonte;
+	String link;
+	String immagine;
+	
+	public Notizia() {
+		
+	}
+	
+	public Notizia(String titolo,LocalDateTime timestamp,String descrizione,String autore,String fonte,String link,String immagine) {
+		this.titolo=titolo;
+		this.timestamp=timestamp;
+		this.descrizione=descrizione;
+		this.autore=autore;
+		this.fonte=fonte;
+		this.link=link;
+		this.immagine=immagine;
+		
+	}
+	public String getTitolo() {
+		return titolo;
+	}
+	
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public String getAutore() {
+		return autore;
+	}
+	public String getFonte() {
+		return fonte;
+	}
+	public String getLink() {
+		return link;
+	}
+	public String getImmagine() {
+		return immagine;
+	}
+
+}
---
+++ NABOO/src/application/Utente.java
@@ -0,0 +1,38 @@
+package application;
+
+public class Utente {
+	private String nome;
+	private String cognome;
+	private String username;
+	
+	public Utente(String nome, String cognome, String username) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+
+}
