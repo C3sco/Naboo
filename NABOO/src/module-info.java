c18a9752f250d0c1f2192c93da681b7bde24fe2c
C3sco <c3sco1@gmail.com>
2022-09-11 18:06:25 +0200

    FUNZIONA

--- NABOO/src/module-info.java
+++ NABOO/src/module-info.java
@@ -2,12 +2,14 @@ module NABOO {
 	requires javafx.base;
 	requires javafx.graphics;
 	requires javafx.controls;
-	requires telegrambots;
-	requires telegrambots.meta;
 	requires com.google.gson;
 	requires javafx.fxml;
 	requires rome;
-	requires slf4j;
+	requires telegrambots;
+	requires telegrambots.meta;
+	requires com.fasterxml.jackson.databind;
+
+	
 
 	opens application to javafx.graphics, javafx.fxml, com.google.gson, javafx.base;
 }
